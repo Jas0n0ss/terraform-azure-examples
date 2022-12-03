6e3a769a2129eece893fc548d81ad66b353f6bf7
Jason Hu <job.bo@outlook.com>
2022-12-04 00:28:20 +0800

    update azure vm image to centos & ssh-key

--- azure-vm/output.tf
+++ azure-vm/output.tf
@@ -1,12 +1,15 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.main.name
 }
-
+output "computer_name" {
+  value = azurerm_linux_virtual_machine.main.computer_name
+}
+output "admin_username" {
+  value = azurerm_linux_virtual_machine.main.admin_username
+}
 output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
+  value = azurerm_linux_virtual_machine.main.public_ip_address
 }
-
-output "tls_private_key" {
-  value     = tls_private_key.example_ssh.private_key_pem
-  sensitive = true
+output "admin_ssh_key" {
+  value = azurerm_linux_virtual_machine.main.admin_ssh_key
 }
